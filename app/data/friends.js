9e2ac62a1195e28b128d7ad7bf88feb5774d9a67
slayte <slayteeaton@gmail.com>
2018-12-02 15:20:32 -0600

    Completed writing the questions.

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+let friendsArray = [];
+
+function Friend(name, photo, scores){
+    this.name = name;
+    this.photo = photo;
+    this.scores = scores;
+}
+
+// let friend = new Friend("sasha", "photo link", [3,4,5]);
+// console.log(friend);
+
+function newFriend (result){
+    let friend = new Friend(result.name, result.photo, result.scores);
+    friendsArray.push(friend);
+}
