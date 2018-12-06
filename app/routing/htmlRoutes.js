74d83b579330231dddd8cf7fd8c074a67cc40fbe
slayte <slayteeaton@gmail.com>
2018-12-05 18:31:31 -0600

    Worked on form validation inside the script tags of the survey html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,12 +5,12 @@ var path = require("path");
 module.exports = function(app){
 
     //GET route to "./public/survey.html" which should display the survey page
-    app.get("/", function(req, res){
+    app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     })
 
     //default, catch-all route to "./public/home.html" which displays the home page
-    app.get("/*", function(req, res){
+    app.get("/", function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
